11c49da6aacd6ddc4f2cf31e182f6cdc095af367
inkanimal <theatomproject@gmail.com>
2019-09-16 12:33:50 -0400

    Update sessions_controller.rb

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create
-        @user = User.find_by
+        @user = User.find_by(username: params[:session][:username])
+
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            render json: UserSerializer.new(@user), status: :ok
+        else
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            redner json: UserSerializer.new(current_user)
+        else
+            render json: {
+                error: "No one is logged in."
+            }
+        end
     end
 
 end
