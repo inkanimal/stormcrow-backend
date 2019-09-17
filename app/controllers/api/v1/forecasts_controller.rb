6ba803e5ba0cb02425bed6a483a61fc555879d4c
inkanimal <theatomproject@gmail.com>
2019-09-17 18:23:33 -0400

    update locations controller

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastsController < ApplicationController
-  
+
   def index
+    @forecast = Forecast.all 
+    render json: @forecasts
   end
 
   def show
--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,12 +1,22 @@
 class Api::V1::LocationsController < ApplicationController
-  
+
   def index
+    if logged_in?
+       @location = current_user.locations 
+       render json: @locations
+    else
+      render json: {
+        error: "You must be logged in to view saved locations."
+      }
+    end
   end
 
   def show
+    render json: @location
   end
 
   def create
+    @location = current.user.locations.build(loaction_params)
   end
 
   def update
